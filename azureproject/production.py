a23783ae75719d6e39d9c576912b9b10b560759a
javiglezzz <148767627+javiglezzz@users.noreply.github.com>
2025-05-06 12:07:48 +0000

    prueba push para actualizar azure

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,5 +1,5 @@
 import os
-
+# This is the production configuration file for the Azure project.
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
      dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
      dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
